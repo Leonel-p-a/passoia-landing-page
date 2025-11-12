2d5aa2397afa346c949b220b6a63317399542e65
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-11-12 10:37:09 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import './header.scss';
+
+function Header() {
+    return (
+        <header>
+            <img src="" alt="Logo da marca da página" />
+
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADES</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
