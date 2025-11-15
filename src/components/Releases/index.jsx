707b9390a2a319cf189b79560bb8deee4f0fe558
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-11-15 17:11:15 -0300

    style: Releases styled

--- src/components/Releases/index.jsx
+++ src/components/Releases/index.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import './releases.scss';
 
 import blue from '../../assets/images/azul.png'
 import base from '../../assets/images/base.png'
@@ -16,39 +18,51 @@ import lipstickThree from '../../assets/images/kit_3_batom_em_bala_oceane_editio
 function Releases() {
     const [color, setColor] = useState(blue);
 
-    // const changeColor = () => {
-    //     setColor((prevColor) => {
-    //         if (prevColor === azul) setColor(azul);
-    //         if (prevColor === vermelho) setColor(vermelho)
-    //         if (prevColor === marrom) setColor(marrom)
-    //         if (prevColor === base) setColor(base)
-    //     })
-    // }
+    const [isMobile, setIsMobile] = useState(true);
+
+    const checkIsMobile = () => {
+        const mobileBreakpoint = 768;
+        setIsMobile(window.innerWidth < mobileBreakpoint);
+    };
+
+    useEffect(() => {
+        checkIsMobile();
+
+        window.addEventListener('resize', checkIsMobile);
+
+        return () => {
+            window.removeEventListener('resize', checkIsMobile);
+        };
+    }, []);
 
     return (
-        <section className='releases-section'>
+        <section className='container releases-section p-4'>
             <h2>APROVEITE OS LANÇAMENTOS</h2>
 
-            <div className='lipsticks-container'>
-                <img src={lipstickOne} alt="Imagem de batons" />
-                <img src={lipstickTwo} alt="Imagem de batons" />
-                <img src={lipstickThree} alt="Imagem de batons" />
-            </div>
-
-            <img src={color} alt="Mulher com batom" />
-
-            <div className='details'>
-                <img src={stars} alt="Estrelinhas" />
-                <h3>Matte Premium</h3>
-                <span>Cores disponíveis</span>
-                <img src={blueCircle} onMouseOver={() => setColor(blue)} alt="Batom azul" />
-                <img src={redCircle} onMouseOver={() => setColor(red)} alt="Batom vermelho" />
-                <img src={brownCircle} onMouseOver={() => setColor(brown)} alt="Batom marrom" />
-                <img src={baseCircle} onMouseOver={() => setColor(base)} alt="Batom base" />
-                <h4>Descrição</h4>
-                <p>
-                    O Batom Matte possui uma fórmula inovadora desenvolvida para entregar o máximo de cor na primeira aplicação com um deslize suave e macio. Tem acabamento matte aveludado e manteiga de manga que ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda na hidratação dos lábios, textura fina e macia que não pesa nos lábios.
-                </p>
+            <div className="content">
+                {!isMobile && (
+                    <div className='lipsticks-container'>
+                        <img src={lipstickOne} alt="Imagem de batons" />
+                        <img src={lipstickTwo} alt="Imagem de batons" />
+                        <img src={lipstickThree} alt="Imagem de batons" />
+                    </div>
+                )}
+                <img className='woman' src={color} alt="Mulher com batom" />
+                <div className='details'>
+                    <img className='stars-image' src={stars} alt="Estrelinhas" />
+                    <h3>Matte Premium</h3>
+                    <span>Cores disponíveis</span>
+                    <div className="colors">
+                        <img src={blueCircle} onMouseOver={() => setColor(blue)} alt="Batom azul" />
+                        <img src={redCircle} onMouseOver={() => setColor(red)} alt="Batom vermelho" />
+                        <img src={brownCircle} onMouseOver={() => setColor(brown)} alt="Batom marrom" />
+                        <img src={baseCircle} onMouseOver={() => setColor(base)} alt="Batom base" />
+                    </div>
+                    <h4>Descrição</h4>
+                    <p>
+                        O Batom Matte possui uma fórmula inovadora desenvolvida para entregar o máximo de cor na primeira aplicação com um deslize suave e macio. Tem acabamento matte aveludado e manteiga de manga que ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda na hidratação dos lábios, textura fina e macia que não pesa nos lábios.
+                    </p>
+                </div>
             </div>
         </section>
     )
